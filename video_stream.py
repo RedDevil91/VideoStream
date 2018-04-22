9385400d221c550d20880b18e1e59ff689594055
RedDevil91 <b.attila91@gmail.com>
2018-04-22 17:59:33 +0200

    involve connection into the try-except block

--- video_stream.py
+++ video_stream.py
@@ -8,11 +8,11 @@ HOST = socket.gethostbyname("DESKTOP-S87U62L")
 cap = cv2.VideoCapture(0)
 
 while True:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((HOST, PORT))
-    ret, frame = cap.read()
-    _, img_data = cv2.imencode(".jpg", frame)
     try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        ret, frame = cap.read()
+        _, img_data = cv2.imencode(".jpg", frame)
         sock.sendall(str(len(img_data)).encode())
         response = sock.recv(1024)
         if response == "OK":
