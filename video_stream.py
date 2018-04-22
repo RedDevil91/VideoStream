a87344f2b29b931662ab233b4a2da513b93532f1
RedDevil91 <b.attila91@gmail.com>
2018-04-22 18:37:43 +0200

    use create_connection instead of connect

--- video_stream.py
+++ video_stream.py
@@ -2,15 +2,13 @@ import cv2
 import socket
 
 PORT = 9999
-# HOST = "127.0.0.1"
 HOST = socket.gethostbyname("DESKTOP-S87U62L")
 
 cap = cv2.VideoCapture(0)
 
 while True:
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect((HOST, PORT))
+        sock = socket.create_connection((HOST, PORT), 10)
         ret, frame = cap.read()
         _, img_data = cv2.imencode(".jpg", frame)
         sock.sendall(str(len(img_data)).encode())
@@ -19,6 +17,5 @@ while True:
             sock.sendall(img_data)
             sock.recv(1024)
     except socket.error:
-        sock.close()
         break
 cap.release()
