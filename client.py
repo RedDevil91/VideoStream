e4d864e17f6c37b1ddf6e8f12b50c95abc5951dc
RedDevil91 <b.attila91@gmail.com>
2018-04-22 17:22:54 +0200

    change to socketserver communication

--- client.py
+++ client.py
@@ -2,33 +2,56 @@
 import socket
 import cv2
 import numpy as np
+import SocketServer
+import threading
+import Queue
 
 MAX_RCV_LENGTH = 2048
 
 HOST = 'localhost'    # The remote host
-PORT = 8080           # The same port as used by the server
+PORT = 9999           # The same port as used by the server
 
-stream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-stream.connect((HOST, PORT))
-
-
-def get_image_from_stream(stream_socket, msg_length):
-    chunks = []
-    bytes_rcvd = 0
-    while bytes_rcvd < msg_length:
-        chunk = stream_socket.recv(min(msg_length - bytes_rcvd, MAX_RCV_LENGTH))
-        bytes_rcvd += len(chunk)
-        chunks.append(chunk)
-    data = "".join(chunks)
-    img_data = np.fromstring(data, dtype=np.uint8)
-    img = cv2.imdecode(img_data, 1)
-    return img
+incoming_images = Queue.Queue()
 
 
-while True:
-    msg_length = int(stream.recv(8))
-    received_img = get_image_from_stream(stream, msg_length)
-    cv2.imshow("test", received_img)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-stream.close()
+class ThreadedRequestHandler(SocketServer.BaseRequestHandler):
+    def handle(self):
+        length = int(self.request.recv(1024))
+        self.request.sendall("OK")
+        image = self.get_image_from_stream(length)
+        self.request.sendall("OK")
+
+    def get_image_from_stream(self, image_length):
+        chunks = []
+        bytes_rcvd = 0
+        while bytes_rcvd < image_length:
+            chunk = self.request.recv(min(image_length - bytes_rcvd, MAX_RCV_LENGTH))
+            bytes_rcvd += len(chunk)
+            chunks.append(chunk)
+        data = "".join(chunks)
+        img_data = np.fromstring(data, dtype=np.uint8)
+        img = cv2.imdecode(img_data, 1)
+        return img
+
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
+
+
+if __name__ == '__main__':
+    server = ThreadedTCPServer((HOST, PORT), ThreadedRequestHandler)
+
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.setDaemon(True)
+    server_thread.start()
+
+    while True:
+        if not incoming_images.empty():
+            received_img = incoming_images.get()
+            # msg_length = int(stream.recv(8))
+            # received_img = get_image_from_stream(stream, msg_length)
+            cv2.imshow("test", received_img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    server.shutdown()
+    server.server_close()
--- video_stream.py
+++ video_stream.py
@@ -1,24 +1,23 @@
 import cv2
 import socket
 
-PORT = 8080
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("localhost", PORT))
-s.listen(1)
-
-client_socket, address = s.accept()
+PORT = 9999
+HOST = "127.0.0.1"
 
 cap = cv2.VideoCapture(0)
 
 while True:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    ret, frame = cap.read()
+    _, img_data = cv2.imencode(".jpg", frame)
     try:
-        ret, frame = cap.read()
-        _, img_data = cv2.imencode(".jpg", frame)
-        client_socket.sendall(str(len(img_data)).encode())
-        client_socket.sendall(img_data)
-    except socket.error:
-        break
+        sock.sendall(str(len(img_data)).encode())
+        response = sock.recv(1024)
+        if response == "OK":
+            sock.sendall(img_data)
+            sock.recv(1024)
+    finally:
+        sock.close()
 
-client_socket.close()
 cap.release()
