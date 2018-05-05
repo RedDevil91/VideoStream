d6f3549038eb4d32963f8134f01c1e3970e0bb57
RedDevil91 <b.attila91@gmail.com>
2018-05-05 15:36:29 +0200

    prevent socket creation on every loop

--- video_stream.py
+++ video_stream.py
@@ -3,12 +3,13 @@ import socket
 
 PORT = 9999
 HOST = socket.gethostbyname("DESKTOP-S87U62L")
+# HOST = socket.gethostbyname("raspberrypi")
 
 cap = cv2.VideoCapture(0)
+sock = socket.create_connection((HOST, PORT), 10)
 
 while True:
     try:
-        sock = socket.create_connection((HOST, PORT), 10)
         ret, frame = cap.read()
         _, img_data = cv2.imencode(".jpg", frame)
         sock.sendall(str(len(img_data)).encode())
